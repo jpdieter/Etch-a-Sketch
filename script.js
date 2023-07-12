c37598c20ce59f6ae3c923deffb07bb768dd0176
jpdieter <justindieter@gmail.com>
2023-07-11 22:23:41 -0700

    Created first div

---
+++ script.js
@@ -0,0 +1,8 @@
+//create divs
+var div = document.createElement("div");
+div.style.width = "16px";
+div.style.height = "16px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+document.getElementById("main").appendChild(div);
