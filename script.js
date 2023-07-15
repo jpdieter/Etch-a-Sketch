d0f6d32bf3dc2cb3963bf631c14c10c480bed9c3
jpdieter <justindieter@gmail.com>
2023-07-15 12:30:35 -0700

    Changed button functionality. Created function called 'generateGrid' to generate new grid when button is pressed.

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
 container.appendChild(div);
 }
 
+document.querySelector("#generate-button").addEventListener("click", generate)
 
+function generate(){
+    let generateGrid = prompt("Enter the number of squares per side that you would like to see.");
+    if (generateGrid > 100){
+        alert ("Please select a number less than 100.")
+      
+    }
+}
 
 
