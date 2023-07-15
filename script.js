b4824117ecf9f8bd3cca331361c688a5fffd1dd3
jpdieter <justindieter@gmail.com>
2023-07-15 16:27:16 -0700

    Added default function for 16x16 grid. Modified generateGrid function to take user input and also generate the grid in one function.

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 //create divs
 const container = document.getElementById("container");
-//div.style.width = "10px";
-//div.style.height = "10px";
-//div.style.background = "red";
-//div.style.color = "white";
-//div.innerHTML = "Hello";
-//Add 16 divs
-for (let i = 0; i < 16; i++){
-const div = document.createElement("div");
-div.style.backgroundColor = 'red';
-div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
-container.appendChild(div);
-}
 
-document.querySelector("#generate-button").addEventListener("click", generate)
+defaultGrid();
 
-function generate(){
-    let generateGrid = prompt("Enter the number of squares per side that you would like to see.");
-    if (generateGrid > 100){
-        alert ("Please select a number less than 100.")
-      
+//default function for 16x16 grid
+function defaultGrid(){
+    for (let i = 0; i < 16; i++){
+    const div = document.createElement("div");
+    div.style.backgroundColor = 'black';
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
+    container.appendChild(div);
     }
-}
+    }
+
+//run user choice function when generate button is clicked
+document.querySelector("#generate-button").addEventListener("click", generateGrid)
+
+function generateGrid(){
+    const userChoice = prompt("Enter the number of squares per side that you would like to see.");
+    if (userChoice > 100){
+        alert ("Please select a number less than 100.")
+    }
+    for (i = 0; i < userChoice; i++){
+        const div = document.createElement("div");
+        div.style.backgroundColor = 'red';
+        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
+        container.appendChild(div).className = "generatedDivs";
+}}
+
+
+
+
 
 
