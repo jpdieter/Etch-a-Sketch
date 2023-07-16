1d8fc1985647b9ec531751a6f84391c4f3314c2f
jpdieter <justindieter@gmail.com>
2023-07-15 20:45:17 -0700

    Added clear function but still need to make it work. Attempting to have it clear the default grid when generate button is selected

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 //create divs
-const container = document.getElementById("container");
+// container = document.getElementById("container");
 
-defaultGrid();
 
 //default function for 16x16 grid
-function defaultGrid(){
     for (let i = 0; i < 16; i++){
     const div = document.createElement("div");
     div.style.backgroundColor = 'black';
     div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
     container.appendChild(div);
     }
-    }
+ 
 
 //run user choice function when generate button is clicked
 document.querySelector("#generate-button").addEventListener("click", generateGrid)
 
 function generateGrid(){
-    const userChoice = prompt("Enter the number of squares per side that you would like to see.");
-    if (userChoice > 100){
+   clearGrid()
+    const userChoice = prompt("Please enter a grid size less than or equal to 100:");
+    if (userChoice === undefined || userChoice === null || userChoice === "" || userChoice > 100){
         alert ("Please select a number less than 100.")
+        return
     }
     for (i = 0; i < userChoice; i++){
         const div = document.createElement("div");
         div.style.backgroundColor = 'red';
         div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
-        container.appendChild(div).className = "generatedDivs";
-}}
+        container.appendChild(div).className = "newDiv";
 
+      }}
+
+      //Trying to clear the original function
+      function clearGrid(){
+        let container = document.querySelectorAll("containers");
+        container.forEach(container => {container.style.backgroundColor = null});
+    }
 
 
 
