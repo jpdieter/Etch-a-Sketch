e8c710a4d9437adddef20b57f992b74d329fdc94
jpdieter <justindieter@gmail.com>
2023-07-20 20:22:02 -0700

    Created grid function

---
+++ script1.js
@@ -0,0 +1,24 @@
+document.querySelector("#generate-button").addEventListener("click", generateGrid)
+
+generateGrid(32);
+
+function generateGrid(size){
+ 
+    let container = document.querySelector(".container");
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        container.insertAdjacentElement("beforeend", div);
+    }
+}
+
+reset = document.getElementById("reset-button");
+reset.onclick = function resetGrid(){
+
+}
+
