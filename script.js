5bc4ea5365ef97f2dc9d451b36f4f1c637024e5d
jpdieter <justindieter@gmail.com>
2023-07-23 11:46:16 -0700

    added randomColor function for option to change canvas color

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.querySelector("#generate-button").addEventListener("click", userSelecti
 generateGrid(16);
 
 function generateGrid(size){
-
     let container = document.querySelector(".container");
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -22,7 +21,18 @@ function generateGrid(size){
 reset = document.getElementById("reset-button");
 reset.onclick = function resetGrid(){
     let divs = document.querySelectorAll("div")
-    divs.forEach((div) => div.style.backgroundColor = "#00FF41")
+    divs.forEach((div) => div.style.backgroundColor = "")
+}
+
+colors = ['red', 'blue', 'purple', 'yellow', 'cyan', 'orange', '#4CAF50'];
+randomize = document.getElementById("canvas-button");
+randomize.onclick = function randomColor(){
+    let divs = document.getElementsByClassName("container")
+    let i = divs;
+    for (i = 0; i < divs.length; i++){
+        let randomColor = Math.floor(Math.random()*colors.length)
+        divs[i].style.backgroundColor = colors[randomColor];
+    }
 }
 
 function userSelection(){
@@ -31,6 +41,7 @@ function userSelection(){
         alert ("Please select a number less than 100.")
         return
     }
-   generateGrid(userChoice);
+   
+    generateGrid(userChoice);
 }
 
