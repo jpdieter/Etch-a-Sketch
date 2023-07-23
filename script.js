a59de2901e9d0df5fc016b75c2d27def2afeca41
jpdieter <justindieter@gmail.com>
2023-07-23 10:21:40 -0700

    renamed all files

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
-//create divs
-const container = document.getElementById("container");
+document.querySelector("#generate-button").addEventListener("click", userSelection)
 
-makeRows(16, 16); 
+generateGrid(16);
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.addEventListener("mouseover", function(event){
-            event.target.style.backgroundColor = "black";
-        } )
-        container.appendChild(cell).className = "grid-item";    
-        
-        }
-             
+function generateGrid(size){
 
+    let container = document.querySelector(".container");
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = "black";
+        })
+        container.insertAdjacentElement("beforeend", div);
     }
+}
 
-//default function for 16x16 grid
- //   for (let i = 0; i < 16; i++){
-   // const div = document.createElement("div");
-   // div.style.backgroundColor = 'black';
-   // div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
-   // container.appendChild(div).className = "grid-item";
-   // }
- 
+reset = document.getElementById("reset-button");
+reset.onclick = function resetGrid(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "#00FF41")
+}
 
-//run user choice function when generate button is clicked
-document.querySelector("#generate-button").addEventListener("click", generateGrid)
-
-function generateGrid(){
-    const userChoice = prompt("Please enter a grid size less than or equal to 100:");
-    if (userChoice === undefined || userChoice === null || userChoice === "" || userChoice > 100){
+function userSelection(){
+    let userChoice = prompt("Please enter a grid size less than or equal to 100:");
+    if (userChoice <= undefined || userChoice === null || userChoice === "" || userChoice > 100){
         alert ("Please select a number less than 100.")
         return
     }
-    //makeRows() should be called instead of this code
-    for (i = 0; i < userChoice; i++){
-        const div = document.createElement("div");
-        div.style.backgroundColor = 'red';
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;"
-        container.appendChild(div).className = "newDiv";
-
-      }}
-
-
-      //Trying to clear the original function
-      let clear = document.getElementById("clearBtn");
-      clear.onclick = function clearGrid(){
-       var elements = document.getElementsByClassName("grid-item");
-          for (var i = 0; i < elements.length; i++) {
-  
-            elements[i].style.backgroundColor = null;
-  
-        }
-      }
-  
-
-
-
-
-
-
+   generateGrid(userChoice);
+}
 
